ed53a570e1dc4e7a7264068fb94cd6c7778e0f50
PrikolistTheSillyThing <uzun.andrei13@gmail.com>
2026-03-21 16:11:24 +0200

    display helper function added, visited counter added

--- src/main/java/searchingStuff/Graph.java
+++ src/main/java/searchingStuff/Graph.java
@@ -26,37 +26,74 @@ public class Graph {
 
         Queue<Integer> queue = new LinkedList<>();
 
+        int vertexCount = 0;
+        int edgeCount = 0;
+
         visited[start] = true;
         queue.add(start);
 
         while (!queue.isEmpty()) {
 
             int node = queue.poll();
+            vertexCount++;
             System.out.print(node + " ");
 
             for (int neighbor : adjList[node]) {
+                edgeCount++;
                 if (!visited[neighbor]) {
                     visited[neighbor] = true;
                     queue.add(neighbor);
                 }
             }
         }
+
+        System.out.println();
+
+        System.out.println("Visited vertices: " + vertexCount);
+        System.out.println("Processed edges: " + edgeCount);
     }
 
     void dfs(int start) {
         boolean[] visited = new boolean[vertices];
-        dfsVisit(start, visited);
+
+        int[] vertexCount = {0};
+        int[] edgeCount = {0};
+
+        dfsVisit(start, visited, vertexCount, edgeCount);
+
+        System.out.println();
+        System.out.println("Visited vertices: " + vertexCount[0]);
+        System.out.println("Processed edges: " + edgeCount[0]);
     }
 
-    void dfsVisit(int node, boolean[] visited) {
+    void dfsVisit(int node, boolean[] visited, int[] vertexCount, int[] edgeCount) {
+
         visited[node] = true;
         System.out.print(node + " ");
 
+        vertexCount[0]++;
+
         for (int neighbor : adjList[node]) {
+            edgeCount[0]++;
+
             if (!visited[neighbor]) {
-                dfsVisit(neighbor, visited);
+                dfsVisit(neighbor, visited, vertexCount, edgeCount);
             }
         }
+    }
+
+    static void display(Graph g) {
+        System.out.println("BFS traversal starting from node 0:");
+
+        g.bfs(0);
+
+        System.out.println();
+
+        System.out.println("DFS traversal starting from node 0:");
+
+        g.dfs(0);
+
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -69,14 +106,33 @@ public class Graph {
         g.addEdge(1, 4);
         g.addEdge(2, 5);
 
-        System.out.println("BFS traversal starting from node 0:");
+        display(g);
 
-        g.bfs(0);
+        Graph g2 = new Graph(1000);
 
-        System.out.println();
+        for (int i = 0; i < 500; i++) {
+            int left  = 2 * i + 1;
+            int right = 2 * i + 2;
 
-        System.out.println("DFS traversal starting from node 0:");
+            if (left  < 1000) g2.addEdge(i, left);
+            if (right < 1000) g2.addEdge(i, right);
+        }
 
-        g.dfs(0);
+        display(g2);
+
+        Random rand = new Random();
+
+        Graph g3 = new Graph(1000);
+
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (i != j) {
+                    g3.addEdge(i, j);
+                }
+            }
+        }
+
+        display(g3);
+
     }
 }
